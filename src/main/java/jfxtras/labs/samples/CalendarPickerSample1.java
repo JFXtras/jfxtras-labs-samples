498c811f2d2e5c1dbf31fb29b597ead91430bc4b
Tom Eugelink <tbee@tbee.org>
2013-10-24 20:10:51 +0200

    slowly but surely

--- src/main/java/jfxtras/labs/samples/CalendarPickerSample1.java
+++ src/main/java/jfxtras/labs/samples/CalendarPickerSample1.java
@@ -1,13 +1,27 @@
 package jfxtras.labs.samples;
 
 import fxsampler.SampleBase;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.util.StringConverter;
+import jfxtras.labs.internal.scene.control.skin.CalendarPickerControlSkin;
+import jfxtras.labs.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.control.ChoiceBox;
 import jfxtras.labs.scene.control.CalendarPicker;
 import jfxtras.labs.scene.layout.HBox;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.Observable;
 
 public class CalendarPickerSample1 extends SampleBase
 {
@@ -18,7 +32,7 @@ public class CalendarPickerSample1 extends SampleBase
 
     @Override
     public String getSampleName() {
-        return this.getClass().getName();
+        return this.getClass().getSimpleName();
     }
 
     @Override
@@ -38,12 +52,52 @@ public class CalendarPickerSample1 extends SampleBase
 
     @Override
     public Node getControlPanel() {
-        HBox root = new HBox(20);
-        root.setPadding(new Insets(30, 30, 30, 30));
+        // the result
+        GridPane lGridPane = new GridPane();
+        lGridPane.setVgap(2.0);
+        lGridPane.setHgap(2.0);
 
-        root.getChildren().addAll(new Label("TEST"));
+        // setup the grid so all the labels will not grow, but the rest will
+        ColumnConstraints lColumnConstraintsAlwaysGrow = new ColumnConstraints();
+        lColumnConstraintsAlwaysGrow.setHgrow(Priority.ALWAYS);
+        ColumnConstraints lColumnConstraintsNeverGrow = new ColumnConstraints();
+        lColumnConstraintsNeverGrow.setHgrow(Priority.NEVER);
+        lGridPane.getColumnConstraints().addAll(lColumnConstraintsNeverGrow, lColumnConstraintsAlwaysGrow);
+        int lRowIdx = 0;
+        // Mode
+        {
+            lGridPane.add(new Label("Mode"), new GridPane.C().row(lRowIdx).col(0));
+            ChoiceBox<CalendarPicker.Mode> lChoiceBox = new ChoiceBox(FXCollections.observableArrayList(CalendarPicker.Mode.values()));
+            lGridPane.add(lChoiceBox, new GridPane.C().row(lRowIdx).col(1));
+            lChoiceBox.valueProperty().bindBidirectional(calendarPicker.modeProperty());
+        }
+        lRowIdx++;
 
-        return root;
+        // Locale
+        {
+            lGridPane.add(new Label("Locale"), new GridPane.C().row(lRowIdx).col(0));
+            ObservableList<Locale> lLocales = FXCollections.observableArrayList(Locale.getAvailableLocales());
+            FXCollections.sort(lLocales,  (o1, o2) -> { return o1.toString().compareTo(o2.toString()); } );
+            ComboBox<Locale> lComboBox = new ComboBox( lLocales );
+            lComboBox.converterProperty().set(new StringConverter<Locale>() {
+                @Override
+                public String toString(Locale locale) {
+                    return locale == null ? "-"  : locale.toString();
+                }
+
+                @Override
+                public Locale fromString(String s) {
+                    if ("-".equals(s)) return null;
+                    return new Locale(s);
+                }
+            });
+            lComboBox.setEditable(true);
+            lGridPane.add(lComboBox, new GridPane.C().row(lRowIdx).col(1));
+            lComboBox.valueProperty().bindBidirectional(calendarPicker.localeProperty());
+        }
+        lRowIdx++;
+
+        return lGridPane;
     }
 
     @Override
