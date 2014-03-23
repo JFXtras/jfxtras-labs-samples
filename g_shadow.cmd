d587efacf720df7396a79446c9f269a34423bbd6
Tom Eugelink <tbee@tbee.org>
2014-03-23 21:18:25 +0100

    ...

--- g_shadow.cmd
+++ g_shadow.cmd
@@ -1,2 +1,2 @@
-"c:\Program Files\Java\jre8\bin\java.exe" -jar C:\Users\user\Documents\jfxtras-labs-samples\build\distributions\jfxtras-labs-samples-8.0-r1-SNAPSHOT-shadow.jar
+"c:\Program Files\Java\jre8\bin\java.exe" -jar C:\Users\user\Documents\jfxtras-labs-samples\build\distributions\jfxtras-labs-samples-8.0-r2-SNAPSHOT-shadow.jar
 pause
