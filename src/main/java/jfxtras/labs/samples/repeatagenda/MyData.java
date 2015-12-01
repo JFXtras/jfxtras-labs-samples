db52f64d402b498790d7ce78162277dd82ba3a57
daviddbal <ddbal1@yahoo.com>
2015-12-01 13:58:40 -0800

    enable VEvents

--- src/main/java/jfxtras/labs/samples/repeatagenda/MyData.java
+++ src/main/java/jfxtras/labs/samples/repeatagenda/MyData.java
@@ -5,9 +5,9 @@ import java.util.Set;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import jfxtras.labs.repeatagenda.scene.control.repeatagenda.Repeat;
 import jfxtras.labs.repeatagenda.scene.control.repeatagenda.RepeatableAgenda;
 import jfxtras.labs.repeatagenda.scene.control.repeatagenda.RepeatableAgenda.RepeatableAppointment;
+import jfxtras.labs.repeatagenda.scene.control.repeatagenda.icalendar.VComponent;
 import jfxtras.scene.control.agenda.Agenda.AppointmentGroup;
 
 public final class MyData {
@@ -19,11 +19,11 @@ public final class MyData {
     
 //    private final ObservableList<AppointmentGroup> appointmentGroups = FXCollections.observableArrayList();
     private final ObservableList<AppointmentGroup> appointmentGroups = RepeatableAgenda.DEFAULT_APPOINTMENT_GROUPS;
-    public ObservableList<AppointmentGroup> getAppointmentGroups() { return appointmentGroups; }
-    public void setAppointmentGroups( ObservableList<AppointmentGroup> list) { appointmentGroups.addAll(list); }
+    public ObservableList<AppointmentGroup> appointmentGroups() { return appointmentGroups; }
+//    public void setAppointmentGroups( ObservableList<AppointmentGroup> list) { appointmentGroups.addAll(list); }
 
     /** All appointment repeat rules */
-    private final Set<Repeat> repeats = new HashSet<Repeat>();
-    public Set<Repeat> getRepeats() { return repeats; }
+    private final Set<VComponent> repeats = new HashSet<VComponent>();
+    public Set<VComponent> getVComponents() { return repeats; }
     
 }
