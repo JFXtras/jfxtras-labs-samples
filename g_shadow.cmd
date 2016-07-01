1df790b8cf686450e2ea13434d5e70f90d7ec425
Tom Eugelink <tbee@tbee.org>
2016-07-01 08:08:06 +0200

    released r6

--- g_shadow.cmd
+++ g_shadow.cmd
@@ -1,2 +1,2 @@
-"c:\Program Files\Java\jre1.8.0_40\bin\java.exe" -jar C:\Users\user\Documents\jfxtras-labs-samples\build\libs\jfxtras-labs-samples-8.0-r5-SNAPSHOT-all.jar
+"c:\Program Files\Java\jre1.8.0_40\bin\java.exe" -jar C:\Users\user\Documents\jfxtras-labs-samples\build\libs\jfxtras-labs-samples-8.0-r6-SNAPSHOT-all.jar
 pause
